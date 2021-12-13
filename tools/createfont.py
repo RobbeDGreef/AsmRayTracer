cd8f7b423705ff47b871a7b121a09c836807c005
Awper-Dev <awperdev@gmail.com>
2021-12-13 23:56:51 +0100

    Added tool to create cursor binary

--- tools/createfont.py
+++ tools/createfont.py
@@ -1,10 +1,12 @@
 import os
-import font
+import cursor
+#import font
 
-fontfile = "FONT.BIN"
+#fontfile = "FONT.BIN"
+cursorfile = "CURSOR.BIN"
 
-with open(fontfile, "wb+") as f:
-    for i in range(32):
-        f.write(bytearray([0 for x in range(16)]))
-    f.write(bytearray(font.MonospaceFont16))
+with open(cursorfile, "wb+") as f:
+    #for i in range(32):
+    #    f.write(bytearray([0 for x in range(16)]))
+    f.write(bytearray(cursor.cursor))
     f.close()
